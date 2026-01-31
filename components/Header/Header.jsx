c84231d0a1e4bae4342e695543c645d5047ee8b8
Othman Ahmed <othmanahmed258@gmail.com>
2026-01-31 07:22:10 +0200

    create: button animation

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import NavButton from "./NavButton/NavButton";
 
 const Header = () => {
+  const [isActive, setIsActive] = useState(false);
   return (
     <div>
-      <NavButton />
+      <NavButton isActive={isActive} setIsActive={setIsActive} />
     </div>
   );
 };
--- components/Header/NavButton/NavButton.jsx
+++ components/Header/NavButton/NavButton.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+"useClient";
 
-const NavButton = () => {
+import gsap from "gsap";
+
+const NavButton = ({ isActive, setIsActive }) => {
+  const handleButtonClick = () => {
+    gsap.to(".slider", { y: isActive ? -40 : 0 });
+  };
+
   return (
-    <div className="fixed top-[2rem] right-[2rem] w-[6rem] h-[2.5rem] bg-[#c9fd74] rounded-[1.5rem] cursor-pointer">
-      <div className="bg-red-400 w-full h-full flex items-center justify-center">
-        Menu
-      </div>
-      <div className="bg-green-400 w-full h-full flex items-center justify-center">
-        Close
+    <div
+      onClick={() => setIsActive(!isActive)}
+      className="fixed top-[2rem] right-[2rem] w-[6rem] h-[2.5rem] rounded-[1.5rem] cursor-pointer overflow-hidden"
+    >
+      <div onClick={handleButtonClick} className="slider w-full h-full">
+        <div className="bg-[#c9fd74] w-full h-full flex items-center justify-center">
+          Menu
+        </div>
+        <div className="bg-black text-white w-full h-full flex items-center justify-center">
+          Close
+        </div>
       </div>
     </div>
   );
