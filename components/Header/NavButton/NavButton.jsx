2f0cd8b69dfefd4ef78683d886caaf978a9252a7
Othman Ahmed <othmanahmed258@gmail.com>
2026-01-31 08:52:52 +0200

    create: text animation for nav button text

--- components/Header/NavButton/NavButton.jsx
+++ components/Header/NavButton/NavButton.jsx
@@ -3,15 +3,19 @@
 import gsap from "gsap";
 import CustomEase from "gsap/dist/CustomEase";
 import NavLabel from "../NavLabel/NavLabel";
+import { useRef } from "react";
 
 const NavButton = ({ isActive, setIsActive }) => {
+  const sliderRef = useRef(null);
+
   gsap.registerPlugin(CustomEase);
+  let navEase = CustomEase.create("navButton", "0.76, 0, 0.24, 1");
 
   const handleButtonClick = () => {
     gsap.to(".slider", {
       y: isActive ? "-100%" : 0,
       duration: 0.5,
-      ease: CustomEase.create("navButton", "0.76, 0, 0.24, 1"),
+      ease: navEase,
     });
   };
 
@@ -22,10 +26,10 @@ const NavButton = ({ isActive, setIsActive }) => {
     >
       <div onClick={handleButtonClick} className="slider w-full h-full">
         <div className="bg-[#c9fd74] w-full h-full flex items-center justify-center">
-          <NavLabel label={"menu"} />
+          <NavLabel label={"menu"} ease={navEase} />
         </div>
         <div className="bg-black text-white w-full h-full flex items-center justify-center">
-          <NavLabel label={"close"} />
+          <NavLabel label={"close"} ease={navEase} />
         </div>
       </div>
     </div>
--- components/Header/NavLabel/NavLabel.jsx
+++ components/Header/NavLabel/NavLabel.jsx
@@ -1,10 +1,39 @@
+import gsap from "gsap";
+import SplitText from "gsap/dist/SplitText";
 import React from "react";
 
-const NavLabel = ({ label }) => {
+const NavLabel = ({ label, ease }) => {
+  gsap.registerPlugin(SplitText);
+
+  const handleHover = () => {
+    let split1 = SplitText.create(".label-1", {
+      type: "chars",
+    });
+    let split2 = SplitText.create(".label-2", {
+      type: "chars",
+    });
+
+    let tl = gsap.timeline();
+
+    tl.to(split1.chars, { y: "-100%", stagger: 0.1, ease: ease }).to(
+      split2.chars,
+      {
+        y: "-100%",
+        stagger: 0.1,
+        ease: ease,
+      },
+      "<",
+    );
+  };
+
   return (
-    <div>
-      <p>{label}</p>
-      <p>{label}</p>
+    <div
+      onMouseEnter={handleHover}
+      className="relative flex flex-col items-center justify-center overflow-hidden"
+      style={{ lineHeight: 0.85 }}
+    >
+      <p className="label-1">{label}</p>
+      <p className="label-2 absolute top-[100%]">{label}</p>
     </div>
   );
 };
