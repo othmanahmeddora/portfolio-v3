150c7210ff67886846b2cb30cf193b2f33a835f5
Othman Ahmed <othmanahmed258@gmail.com>
2026-01-31 07:58:39 +0200

    create: NavLabel.jsx file

--- components/Header/NavButton/NavButton.jsx
+++ components/Header/NavButton/NavButton.jsx
@@ -1,23 +1,31 @@
 "useClient";
 
 import gsap from "gsap";
+import CustomEase from "gsap/dist/CustomEase";
+import NavLabel from "../NavLabel/NavLabel";
 
 const NavButton = ({ isActive, setIsActive }) => {
+  gsap.registerPlugin(CustomEase);
+
   const handleButtonClick = () => {
-    gsap.to(".slider", { y: isActive ? -40 : 0 });
+    gsap.to(".slider", {
+      y: isActive ? "-100%" : 0,
+      duration: 0.5,
+      ease: CustomEase.create("navButton", "0.76, 0, 0.24, 1"),
+    });
   };
 
   return (
     <div
       onClick={() => setIsActive(!isActive)}
-      className="fixed top-[2rem] right-[2rem] w-[6rem] h-[2.5rem] rounded-[1.5rem] cursor-pointer overflow-hidden"
+      className="fixed top-[2rem] right-[2rem] w-[7rem] h-[2.5rem] rounded-[1.5rem] cursor-pointer overflow-hidden uppercase font-[500]"
     >
       <div onClick={handleButtonClick} className="slider w-full h-full">
         <div className="bg-[#c9fd74] w-full h-full flex items-center justify-center">
-          Menu
+          <NavLabel label={"menu"} />
         </div>
         <div className="bg-black text-white w-full h-full flex items-center justify-center">
-          Close
+          <NavLabel label={"close"} />
         </div>
       </div>
     </div>
---
+++ components/Header/NavLabel/NavLabel.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavLabel = ({ label }) => {
+  return (
+    <div>
+      <p>{label}</p>
+      <p>{label}</p>
+    </div>
+  );
+};
+
+export default NavLabel;
