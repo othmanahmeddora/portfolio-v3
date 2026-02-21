68626d4ea4b93114303786ba308f8cf35384bd2d
Othman Ahmed <othmanahmed258@gmail.com>
2026-02-22 00:46:03 +0200

    edit: hero image

--- app/page.js
+++ app/page.js
@@ -1,3 +1,9 @@
+import Hero from "@/components/Hero/Hero";
+
 export default function Home() {
-  return <></>;
+  return (
+    <div className="bg-[#f5f5f5] ">
+      <Hero />
+    </div>
+  );
 }
--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -1,7 +1,18 @@
+import Image from "next/image";
 import React from "react";
 
 const Hero = () => {
-  return <section></section>;
+  return (
+    <section className="h-screen max-w-360 px-4 overflow-hidden relative mx-auto">
+      <Image
+        height={500}
+        width={500}
+        alt="Othman Image"
+        src="/img/hero.png"
+        className="grayscale hover:grayscale-0 hover:scale-110 transition-all duration-500 ease-out"
+      />
+    </section>
+  );
 };
 
 export default Hero;
