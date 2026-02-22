d8df714d97d03146c6ace5ad3f7e8a9d7b87bf76
Othman Ahmed <othmanahmed258@gmail.com>
2026-02-22 02:26:19 +0200

    add: text and center the image

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -4,14 +4,20 @@ import React from "react";
 const Hero = () => {
   return (
     <section className="h-screen px-4 overflow-hidden relative mx-auto z-0">
-      <div className="flex justify-center mt-40">
+      <div className="flex justify-center mt-10 z-50">
         <Image
           height={500}
           width={500}
           alt="Othman Image"
           src="/img/hero.png"
-          className="mt-[10rem] grayscale hover:grayscale-0 hover:scale-110 transition-all duration-500 ease-out"
+          className="grayscale w-[40rem]"
         />
+      </div>
+
+      <div className="absolute top-[10%] left-[50%] translate-x-[-50%] text-[20rem] leading-[15rem] uppercase text-center font-[700] z-[-1] text-[#c9fd74]">
+        <p className="tracking-[-0.06em]">Front-end</p>
+        <p className="tracking-[-0.033em]">Developer</p>
+        <p className="tracking-[-0.085em]">Freelancer</p>
       </div>
     </section>
   );
