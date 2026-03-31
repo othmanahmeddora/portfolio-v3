171d8742f00f0c05e317b846b77c59420efcd158
Othman Ahmed <othmanahmed258@gmail.com>
2026-03-31 21:55:35 +0200

    create: counter style and animation

--- components/Preloaders/PageReveal/PageReveal.jsx
+++ components/Preloaders/PageReveal/PageReveal.jsx
@@ -1,14 +1,47 @@
 import gsap from "gsap";
-import React from "react";
+import CustomEase from "gsap/dist/CustomEase";
+import React, { useEffect } from "react";
+
+CustomEase.create("hop", "0.9, 0, 0.1, 1");
 
 const PageReveal = () => {
-  CustomEase.create("hop", "0.9, 0, 0.1, 1");
+  useEffect(() => {
+    const tl = gsap.timeline({ delay: 0.3, defaults: { ease: "hop" } });
+
+    let counter = document.querySelector(".counter");
+    let currentCounterValue = 20;
+
+    const updateCounter = () => {
+      if (currentCounterValue === 100) {
+        return;
+      }
+
+      currentCounterValue += Math.floor(Math.random() * 10);
+
+      if (currentCounterValue > 100 || currentCounterValue === 100) {
+        currentCounterValue = 100;
+        gsap.to(counter, {
+          scale: "500",
+          delay: 0.5,
+          duration: 2,
+          ease: "expo.inOut",
+        });
+      }
+
+      counter.textContent = currentCounterValue;
+
+      let delay = Math.floor(Math.random() * 300);
+
+      setTimeout(updateCounter, delay);
+    };
+    updateCounter();
+  }, []);
   return (
     <section className="relative z-50 h-screen">
       <div className="absolute bg-[#171717] h-screen w-[50%] top-0 left-0"></div>
       <div className="absolute bg-[#171717] h-screen w-[50%] top-0 left-[50%]"></div>
 
-      <h1 className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-[#f5f5f5] text-[15rem]">
+      <h1 className="counter absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-[#f5f5f5] text-[15rem]">
         0
       </h1>
 
