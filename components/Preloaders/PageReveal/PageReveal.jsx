6cf8485c6a838cd77ba147a25bb0694f35a97499
Othman Ahmed <othmanahmed258@gmail.com>
2026-04-01 09:34:04 +0200

    create: name animation

--- components/Preloaders/PageReveal/PageReveal.jsx
+++ components/Preloaders/PageReveal/PageReveal.jsx
@@ -1,13 +1,16 @@
+import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import CustomEase from "gsap/dist/CustomEase";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 CustomEase.create("hop", "0.9, 0, 0.1, 1");
 
 const PageReveal = () => {
-  useEffect(() => {
-    const tl = gsap.timeline({ delay: 0.3, defaults: { ease: "hop" } });
+  const name1 = useRef(null);
+  const name2 = useRef(null);
 
+  useEffect(() => {
+    const tl = gsap.timeline();
     let counter = document.querySelector(".counter");
     let currentCounterValue = 20;
 
@@ -20,12 +23,18 @@ const PageReveal = () => {
 
       if (currentCounterValue > 100 || currentCounterValue === 100) {
         currentCounterValue = 100;
-        gsap.to(counter, {
+        tl.to(counter, {
           scale: "500",
           delay: 0.5,
           duration: 2,
           ease: "expo.inOut",
         });
+
+        tl.to(name1.current, { y: "0%" }, "-=.5");
+        tl.to(name2.current, { y: "0%" }, "<");
+
+        tl.to(name1.current, { y: "100%", delay: 1 });
+        tl.to(name2.current, { y: "-100%" }, "<");
       }
 
       counter.textContent = currentCounterValue;
@@ -36,6 +45,7 @@ const PageReveal = () => {
     };
     updateCounter();
   }, []);
+
   return (
     <section className="relative z-50 h-screen">
       <div className="absolute bg-[#171717] h-screen w-[50%] top-0 left-0"></div>
@@ -45,9 +55,13 @@ const PageReveal = () => {
         0
       </h1>
 
-      <div className="flex items-center gap-8 absolute top-[50%] left-[50%] translate-x-[-52%] translate-y-[-50%] text-[#f5f5f5] text-[2.5rem]">
-        <h1>othman</h1>
-        <h1>ahmed</h1>
+      <div className="overflow-hidden flex items-center gap-8 absolute top-[50%] left-[50%] translate-x-[-52%] translate-y-[-50%] text-[#f5f5f5] text-[2.5rem]">
+        <h1 ref={name1} className="translate-y-[-120%]">
+          othman
+        </h1>
+        <h1 ref={name2} className="translate-y-[120%]">
+          ahmed
+        </h1>
       </div>
 
       <span className="absolute top-0 left-[50%] translate-x-[-50%] origin-[center top] w-px h-full bg-white"></span>
