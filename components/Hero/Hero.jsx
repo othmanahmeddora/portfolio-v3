4c884bf8c1b0441411f715c2af54a77d38a1b6c2
Othman Ahmed <othmanahmed258@gmail.com>
2026-04-03 18:39:51 +0200

    create: parallax effect in hero section

--- components/Hero/Hero.jsx
+++ components/Hero/Hero.jsx
@@ -71,7 +71,11 @@ const Hero = () => {
       </section>
 
       <section className="h-screen px-4 overflow-hidden relative mx-auto z-0">
-        <div className="flex justify-center mt-10 z-50">
+        <div
+          data-scroll
+          data-scroll-speed="0.2"
+          className="flex justify-center mt-10 z-50"
+        >
           <Image
             height={500}
             width={500}
@@ -81,7 +85,11 @@ const Hero = () => {
           />
         </div>
 
-        <div className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-[20rem] leading-60 uppercase text-center font-bold z-[-1] text-primary">
+        <div
+          data-scroll
+          data-scroll-speed="-0.1"
+          className="absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] text-[20rem] leading-60 uppercase text-center font-bold z-[-1] text-primary"
+        >
           <p ref={title1} className="tracking-[-0.049em] whitespace-nowrap">
             Front-end
           </p>
@@ -97,52 +105,54 @@ const Hero = () => {
           ref={scrollDownSpinner}
           className="absolute bottom-[10%] left-[50%] translate-x-[-50%] z-3 text-light uppercase font-bold"
         >
-          <div className="relative w-35 h-35">
-            <svg
-              viewBox="0 0 140 140"
-              width="140"
-              height="140"
-              className="absolute top-0 left-0 animate-spin-slow"
-            >
-              <defs>
-                <path
-                  id="circlePath"
-                  d="M 70,70 m -46,0 a 46,46 0 1,1 92,0 a 46,46 0 1,1 -92,0"
-                />
-              </defs>
-              <circle cx="70" cy="70" r="38" fill="#c9fd74" />
-              <text
-                fill="#f5f5f5"
-                fontSize="12.5"
-                fontWeight="600"
-                letterSpacing="3.2"
+          <div data-scroll data-scroll-speed="0.4">
+            <div className="relative w-35 h-35">
+              <svg
+                viewBox="0 0 140 140"
+                width="140"
+                height="140"
+                className="absolute top-0 left-0 animate-spin-slow"
               >
-                <textPath href="#circlePath">
-                  . SCROLL DOWN . SCROLL DOWN
-                </textPath>
-              </text>
-            </svg>
-
-            <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
-              <svg width="25" height="30" viewBox="0 0 16 20" fill="none">
-                <line
-                  x1="8"
-                  y1="0"
-                  x2="8"
-                  y2="16"
-                  stroke="#f5f5f5"
-                  strokeWidth="2.5"
-                  strokeLinecap="round"
-                />
-                <polyline
-                  points="2,10 8,17 14,10"
-                  fill="none"
-                  stroke="#f5f5f5"
-                  strokeWidth="2.5"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
+                <defs>
+                  <path
+                    id="circlePath"
+                    d="M 70,70 m -46,0 a 46,46 0 1,1 92,0 a 46,46 0 1,1 -92,0"
+                  />
+                </defs>
+                <circle cx="70" cy="70" r="38" fill="#c9fd74" />
+                <text
+                  fill="#f5f5f5"
+                  fontSize="12.5"
+                  fontWeight="600"
+                  letterSpacing="3.2"
+                >
+                  <textPath href="#circlePath">
+                    . SCROLL DOWN . SCROLL DOWN
+                  </textPath>
+                </text>
               </svg>
+
+              <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
+                <svg width="25" height="30" viewBox="0 0 16 20" fill="none">
+                  <line
+                    x1="8"
+                    y1="0"
+                    x2="8"
+                    y2="16"
+                    stroke="#f5f5f5"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                  />
+                  <polyline
+                    points="2,10 8,17 14,10"
+                    fill="none"
+                    stroke="#f5f5f5"
+                    strokeWidth="2.5"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  />
+                </svg>
+              </div>
             </div>
           </div>
         </div>
